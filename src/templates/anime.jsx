1b8b8b892a368e7a43eeee5f797faa8dfc6ed9f5
XerAnime <160598198+XerAnime@users.noreply.github.com>
2024-03-11 13:50:49 +0800

    Update anime.jsx

--- src/templates/anime.jsx
+++ src/templates/anime.jsx
@@ -63,7 +63,7 @@ function Anime() {
 		<div className='animeMain'>
 			<div>
 				<ul className='navbar'>
-					<li><a href="/">XerStream-Movie</a></li>
+					<li><a href="/">XerStream-Anime</a></li>
 					<input
 						placeholder='Enter anime title '
 						onKeyDown={(event) => handleKeyEvent(event)}
